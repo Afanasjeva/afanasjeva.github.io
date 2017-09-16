bde9e01f9d382270b67a9e83f1f413cc568cee4f
Afanasjeva <tatjana.afanasjeva@gmail.com>
2017-09-16 17:24:41 +0300

    20170916

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
 $(function(){
+
+    $('.grid_item').each(function() {
+        $(this).height($(this).width());
+    });
+    $('.grid_item_vertical').each(function() {
+        $(this).height($(this).width()*2);
+    });
+    $(window).on('resize',function(){
+        $('.grid_item').each(function() {
+            $(this).height($(this).width());
+        });
+        $('.grid').isotope('shuffle');
+    });
     $('.grid').isotope({
       itemSelector: '.grid_item',
-      layoutMode: 'fitRows'
+      layoutMode: 'masonry',
+        cellsByRow: {
+            columnWidth: 200,
+            rowHeight: 200
+        },
+    });
+    $('.filter_link').on('click',function(e){
+        e.preventDefault();
+        var selector = $(this).data('filter');
+        if ( selector !== '*' ) {
+            selector = selector + ', .corner_stamp'
+        }
+        $('.grid').isotope({ filter: selector });
+
     });
 });
