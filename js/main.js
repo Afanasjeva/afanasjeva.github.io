c41cf2498cd00f65b7ac053ca76ee5357c95988b
Afanasjeva <tatjana.afanasjeva@gmail.com>
2017-09-20 15:52:14 +0300

    favicon

--- js/main.js
+++ js/main.js
@@ -1,27 +1,26 @@
 $(function(){
-    $('.grid_item').each(function() {
-        $(this).height($(this).width());
-    });
-    $('.grid_item_vertical').each(function() {
-        $(this).height($(this).width()*2);
-    });
-    $(window).on('resize',function(){
+
+    let gridItemsInit = function(){
         $('.grid_item').each(function() {
             $(this).height($(this).width());
         });
         $('.grid_item_vertical').each(function() {
             $(this).height($(this).width()*2);
         });
+
+    };
+
+    gridItemsInit();
+    $(window).on('resize',function(){
+        gridItemsInit();
         // $('.grid').isotope('shuffle');
     });
+
     $('.grid').isotope({
       itemSelector: '.grid_item',
-      layoutMode: 'masonry',
-        /*cellsByRow: {
-            columnWidth: 200,
-            rowHeight: 200
-        },*/
-    });
+      layoutMode: 'masonry'
+    }).isotope({ filter: '.grid_item' });
+
     $('.filter_link').on('click',function(e){
         e.preventDefault();
         var selector = $(this).data('filter');
@@ -31,13 +30,23 @@ $(function(){
         $('.grid').isotope({ filter: selector });
     });
     // *****************************************************************************************color_link
+    var color;
     $('.color_link').each(function(e){
-        var color = $(this).data('color');
-        $(this).css({'color':color});
+        color = $(this).data('color');
+        $(this).find('i').css({'color':color});
     });
     $('.color_link').on('click',function(e){
        e.preventDefault();
-       var color = $(this).data('color');
+       color = $(this).data('color');
        $(':root').css({'--color_main':color});
     });
+    // *****************************************************************************************slim_scroll
+    /*$('.grid').slimscroll({
+        height: 'auto',
+        size: '5px',
+        color: color,
+        distance: '10px',
+        opacity: 1,
+    });*/
+
 });
