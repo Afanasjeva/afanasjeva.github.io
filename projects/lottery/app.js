081f26f0fbf15ea8844bcaaaefcbd474d0093a3d
Tatjana Afanasjeva <tatjana.afanasjeva@gmail.com>
2018-11-06 11:42:26 +0200

    lottery edit function added

--- projects/lottery/app.js
+++ projects/lottery/app.js
@@ -1,32 +1,43 @@
 var Lottery = function(){
-    var users = [];
+    this.users = [];
     var lottery = this;
     this.$form = document.getElementById('form');
     this.$winnersBtn = document.getElementById('winners-btn');
     this.$users = document.getElementById('users');
     this.$winners = document.getElementById('winners');
-    var possibleInputs = ['name','surname','email','phone','birthday'];
+    this.possibleInputs = ['name','surname','email','phone','birthday'];
 
     this.addUserRow = function(user, container){
         var elem = document.createElement('div');
         elem.classList.add('user-row');
+        elem.setAttribute('id', 'user-row-' + user.id);
 
         var deleteBtn = document.createElement('button');
         deleteBtn.classList.add('delete-btn');
         deleteBtn.innerHTML = 'Delete';
         deleteBtn.addEventListener('click', function(){
             this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);
-            users.splice(user.id, 1);
+            lottery.users.splice(user.id, 1);
         });
-        elem.appendChild(deleteBtn);
-        
-        possibleInputs.forEach(function(el){
+
+        var editBtn = document.createElement('button');
+        editBtn.classList.add('edit-btn');
+        editBtn.innerHTML = 'Edit';
+        editBtn.addEventListener('click', function(){
+            editPopup.open(user);
+        });
+
+        this.possibleInputs.forEach(function(el){
             var subElem = document.createElement('div');
+            subElem.setAttribute('id', 'user-' + user.id + '-prop-' + el);
             subElem.innerHTML = user[el];
             elem.appendChild(subElem);
         });
+
         var subElem = document.createElement('div');
+        subElem.appendChild(editBtn);
         subElem.appendChild(deleteBtn);
+
         elem.appendChild(subElem);
 
         container.appendChild(elem);
@@ -36,20 +47,20 @@ var Lottery = function(){
         this.$form.addEventListener('submit', function(e){
             e.preventDefault();
             var user = {};
-            possibleInputs.forEach(function(el){
+            lottery.possibleInputs.forEach(function(el){
                 user[el] = document.getElementById(el).value;
             });
-            user.id = users.length;
-            users.push(user);
+            user.id = lottery.users.length;
+            lottery.users.push(user);
             lottery.addUserRow(user, lottery.$users);
             lottery.clearForm();
         });
     };
     this.clearForm = function(){
-        possibleInputs.forEach(function(el){
+        this.possibleInputs.forEach(function(el){
             document.getElementById(el).value = '';
         });
-        document.getElementById(possibleInputs[0]).focus();
+        document.getElementById(lottery.possibleInputs[0]).focus();
     };
 
     this.initWinnersBtn = function(){
@@ -59,9 +70,21 @@ var Lottery = function(){
     };
 
     this.newWinner = function(){
-        var index = Math.round(Math.random()*(users.length - 1));
-        users[index].winner = true;
-        lottery.addUserRow(users[index], lottery.$winners);
+        var index = Math.round(Math.random()*(lottery.users.length - 1));
+        lottery.users[index].winner = true;
+        lottery.addUserRow(lottery.users[index], lottery.$winners);
+    };
+
+    this.changeUserRow = function(id){
+        var $row = document.getElementById('user-row-' + id);
+        var user;
+        lottery.users.forEach(function(el){
+            if (el.id === id) user = el;
+        });
+
+        lottery.possibleInputs.forEach(function(el){
+            document.getElementById("user-" + id + "-prop-" + el).innerHTML = user[el];
+        });
     };
 
     this.init = function(){
@@ -71,3 +94,46 @@ var Lottery = function(){
     this.init();
 };
 var lottery = new Lottery();
+
+var EditPopup = function () {
+  self = this;
+  this.$popup = document.getElementById('edit-popup');
+  this.$form = document.getElementById('edit-form');
+  this.$closeBtn = document.getElementById('edit-close');
+  this.open = function(user){
+      this.user = user;
+      lottery.possibleInputs.forEach(function(el){
+          var id = 'edit-'+el;
+          document.getElementById(id).value = user[el];
+      });
+      this.$popup.classList.add('active');
+  };
+  this.close = function(){
+      this.$popup.classList.remove('active');
+  };
+  this.initCloseBtn = function(){
+      this.$closeBtn.addEventListener('click', function(){
+          self.close();
+      });
+  };
+  this.initForm = function(){
+      this.$form.addEventListener('submit', function(e){
+          e.preventDefault();
+          lottery.users.forEach(function(user){
+              if (user.id === self.user.id){
+                  lottery.possibleInputs.forEach(function(el){
+                      user[el] = document.getElementById('edit-'+el).value;
+                  });
+              }
+          });
+          self.close();
+          lottery.changeUserRow(self.user.id);
+      });
+  };
+  this.init = function(){
+      this.initCloseBtn();
+      this.initForm();
+  };
+  this.init();
+};
+var editPopup = new EditPopup();
